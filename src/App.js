d581454c5034f3402d287ea6ac2ca45e20c8a8e4
mss456 <mshahlasaliheen@gmail.com>
2025-12-25 10:17:13 +0530

    Add property JSON data and integrate into the App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import propertiesData from "./data/properties.json";
+
 function App() {
   return(
     <div>
       <h1>Estate Agent App</h1>
+
+      <p>Total properties: {propertiesData.properties.length}</p>
     </div>
   );
 }
